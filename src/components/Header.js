5d21f4b9e53925a4c7cc566961ff0714ad9a9d94
seokjun <sukjune96@gmail.com>
2022-04-14 23:49:17 +0900

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="logo">
+        <img src={`${process.env.PUBLIC_URL}/assets/logo.png`} />
+      </div>
+      <div className="menu">
+        {/* 버튼 */}
+        <div>
+          <h2>홈</h2>
+        </div>
+        <div>
+          <h2>영화</h2>
+        </div>
+        <div>
+          <h2>시리즈</h2>
+        </div>
+      </div>
+      <div className="search">
+        <h4>검색하기</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
